f56b7eb35cbbdbf2512c60085a28e834ed6ad5d4
Yurii <diesel6@i.ua>
2019-05-28 16:13:04 +0300

    added variables blockSize, widthInBlocks, heightInBlocks and score in index.js

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ let ctx = canvas.getContext("2d");
 
 let width = canvas.width;
 let height = canvas.height;
+
+let blockSize = 10;
+let widthInBlocks = width / blockSize;
+let heightInBlocks = height / blockSize;
+
+let score = 0;
+
