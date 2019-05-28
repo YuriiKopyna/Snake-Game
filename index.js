25e071d0c04d2491bb9a41b6683dfe4fdd199f27
Yurii <diesel6@i.ua>
2019-05-28 16:20:32 +0300

    added function drawBorder in index.js

--- index.js
+++ index.js
@@ -10,3 +10,10 @@ let heightInBlocks = height / blockSize;
 
 let score = 0;
 
+function drawBorder() {
+    ctx.fillStyle = "Gray";
+    ctx.fillRect(0, 0, width, blockSize);
+    ctx.fillRect(0, height - blockSize, width, blockSize);
+    ctx.fillRect(0, 0, blockSize, height);
+    ctx.fillRect(width - blockSize, 0, blockSize, height);
+};
