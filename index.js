3dcebdb9d5f1009ff20d7c47c5ab799a95edfe9e
Yurii <diesel6@i.ua>
2019-05-29 10:57:59 +0300

    change all code accordingly to book code

--- index.js
+++ index.js
@@ -1,38 +1,39 @@
-let canvas = document.getElementById("canvas");
-let ctx = canvas.getContext("2d");
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var width = canvas.width;
+var height = canvas.height;
 
-let width = canvas.clientWidth;
-let height = canvas.clientHeight;
+var blockSize = 10;
+var widthInBlocks = width / blockSize;
+var heightInBlocks = height / blockSize;
 
-let blockSize = 10;
-let widthInBlocks = width / blockSize;
-let heightInBlocks = height / blockSize;
+var score = 0;
 
-let score = 0;
+var drawBorder = function () {
+   ctx.fillStyle = "Gray";
+   ctx.fillRect(0, 0, width, blockSize);
+   ctx.fillRect(0, height - blockSize, width, blockSize);
+   ctx.fillRect(0, 0, blockSize, height);
+   ctx.fillRect(width - blockSize, 0, blockSize, height);
+   };
 
-function drawBorder() {
-    ctx.fillStyle = "Gray";
-    ctx.fillRect(0, 0, width, blockSize);
-    ctx.fillRect(0, height-blockSize, width, blockSize);
-    ctx.fillRect(0, 0, blockSize, height);
-    ctx.fillRect(width - blockSize, 0, blockSize, height);
-}
-
-function drawScore() {
+var drawScore = function () {
     ctx.font = "20px Courier";
     ctx.fillStyle = "Black";
     ctx.textAlign = "left";
     ctx.textBaseline = "top";
     ctx.fillText("Score: " + score, blockSize, blockSize);
-}
+   };
 
-function gameOver() {
-    clearInterval(intervalId);
+var gameOver = function () {
+//    clearInterval(intervalId);
     ctx.font = "60px Courier";
     ctx.fillStyle = "Black";
     ctx.textAlign = "center";
     ctx.textBaseline = "middle";
-    ctx.fillText("Game over", width / 2, height / 2);
-}
-
+    ctx.fillText("Game Over", width / 2, height / 2);
+   };
+drawBorder();
+drawScore();
+gameOver();
 
